cf946ada75e04e0f260f55cad4f6224b49b27a51
misko <misko23@gmail.com>
2020-04-23 17:46:53 +0200

    Cleaning order in low disk space mode changed to use done date. Low disk space is declared as 10GB.

--- app/cleaner.py
+++ app/cleaner.py
@@ -1,8 +1,8 @@
 import logging
-import os
 import requests
 import common
 import configuration as cfg
+import sys
 
 
 def cleanup_data():
@@ -11,24 +11,27 @@ def cleanup_data():
     response = requests.post(cfg.TRANSMISSION_API, headers=headers,
                              json={"method": "torrent-get",
                                    "arguments": {
-                                       "fields": ["id", "name", "status", "uploadRatio", "secondsSeeding"]}})
+                                       "fields": ["id", "name", "status", "uploadRatio", "secondsSeeding", "doneDate"]}})
     logging.debug("Response: " + str(response.json()))
 
     processed_items = common.get_processed_list()
-    logging.debug("Disc usage: {:3.2f}".format(get_disc_usage()))
+    logging.debug("Free disk space: {:3.2f}GB".format(get_free_space_in_gb()))
 
     is_any_item_removed = False
-    for item in response.json()["arguments"]["torrents"]:
-        disk_usage = get_disc_usage()
+    items = response.json()["arguments"]["torrents"]
+    items = sorted(items, key=lambda i: i["doneDate"] if i["doneDate"] != 0 else sys.maxsize)
+    for item in items:
+        free_disk_space = get_free_space_in_gb()
         is_processed = item["name"] in processed_items
         is_finished = item["status"] >= 5
         upload_ratio = item["uploadRatio"]
         days_seeding = item["secondsSeeding"] / (60. * 60 * 24)
-        logging.debug("Transmission item: Is Finished={}, Upload Ratio={:3.2f}, Days Seeding={:.2f}, Name={}".format(is_finished, upload_ratio,
-                                                                                                                days_seeding, item["name"]))
+        logging.debug(
+            "Transmission item: Is Finished={}, Upload Ratio={:3.2f}, Days Seeding={:.2f}, Name={}".format(is_finished, upload_ratio,
+                                                                                                           days_seeding, item["name"]))
 
-        if is_processed and is_finished and disk_usage > 90.:
-            logging.info("Item is processed and disk usage is over 90%. Deleting item: {}".format(item["name"]))
+        if is_processed and is_finished and free_disk_space < 10.:
+            logging.info("Item is processed and free disk space is under 10GB. Deleting item: {}".format(item["name"]))
             delete_transmission_item(item)
             is_any_item_removed = True
         elif is_processed and is_finished and (upload_ratio >= 1. or days_seeding > 10.):
@@ -51,8 +54,10 @@ def delete_transmission_item(item):
         logging.warning("Error while removing item from the queue. Error: {}".format(loc_response.text))
 
 
-def get_disc_usage():
-    return 100 - os.statvfs("/").f_bfree / os.statvfs("/").f_blocks * 100
+def get_free_space_in_gb():
+    headers = common.get_transmission_headers()
+    response = requests.post(cfg.TRANSMISSION_API, headers=headers, json={"method": "free-space", "arguments": {"path": "/"}})
+    return response.json()["arguments"]["size-bytes"] / 1024 ** 3
 
 
 if __name__ == '__main__':
